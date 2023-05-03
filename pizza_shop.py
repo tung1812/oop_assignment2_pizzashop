77cb7f0be890877c2a4cc2a87f6f09167d355722
Son <binlabum123@gmail.com>
2023-05-03 14:47:56 +0930

    Assignment starts

---
+++ pizza_shop.py
@@ -0,0 +1,25 @@
+# TODO Claim this as your own work by modifying the following...
+# File: pizza_shop.py 
+# Author: your name...
+# Id: your id... 
+# Description: your program description...
+# This is my own work as defined by the Academic Integrity policy. 
+import math
+
+
+
+# TODO Write your classes here...
+
+
+
+def main():
+    # TODO Write your main program code here...
+    print("~~ Welcome to the Pizza Shop ~~")
+    print()
+
+
+
+# WARNING: Do not write any code in global scope
+
+if __name__ == '__main__':
+    main()
