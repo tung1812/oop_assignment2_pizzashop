81c385425f0d28776585994c98a404ba734191d8
Son <binlabum123@gmail.com>
2023-05-08 01:45:06 +0930

    Unitest for PizzaBase Class

--- pizza_shop.py
+++ pizza_shop.py
@@ -17,41 +17,49 @@ class Food:
 
     def getName(self):
         return self.__name
-    
     def getPrice(self):
+        return self.__price
+    def setPrice(self, price):
+        return price
+    def getStrPrice(self):
         return f"${self.__price:.2f}"
     
-    def clone(self, other):
-        if isinstance(other, Food):
-            return Food(self.__name, self.__price)
+    def clone(self):
+        # return Food(self.__name, self.__price)
+        return type(self)(self.__name, self.__price)
         
     def equalCheck(self, other):
         if isinstance(other, Food):
             return (self.__name == other.__name) and (self.__price == other.__price)
         else: 
             return False
+    def __str__(self):
+        return f"name: {self.__name}, price: {self.__price}"
 
 class PizzaBase(Food):
     def __init__(self, name, price, diameter):
         super().__init__(name, price)
         self.__diameter = diameter
 
-    def clone(self, other):
-        if isinstance(other, PizzaBase):
-            return PizzaBase(self.__name, self.price, self.__diameter)
+    def clone(self):
+        # return PizzaBase(self.getName(), self.getPrice(), self.__diameter)
+        return type(self)(self.getName(), self.getPrice(), self.__diameter)
 
     def equalCheck(self, other):
         if isinstance(other, PizzaBase):
-            return (self.__name == other.__name) and (self.price == other.price) and (self.__diameter == other.__diameter)
+            return (self.getName() == other.getName()) and (self.getPrice() == other.getPrice()) and (self.__diameter == other.__diameter)
 
     def costPerSquareInch(self):
         radius = self.__diameter / 2
-        return self.price / (math.pi * radius**2)
+        return self.getPrice() / (math.pi * radius**2)
+
+    def getDiameter(self):
+        return self.__diameter
 
     def setDiameter(self, diameter):
         costPerSquareInch = self.costPerSquareInch()
         self.__diameter = diameter
-        self.price = costPerSquareInch * math.pi * (diameter / 2)**2
+        self.setPrice(costPerSquareInch * math.pi * (diameter / 2)**2)
 
     def setSize(self, size):
         if size == "small":
@@ -64,12 +72,16 @@ class PizzaBase(Food):
             raise ValueError("Invalid size")
 
     def __str__(self):
-        return f"{self.__name}, {self.__diameter} inch, ${self.price:.2f}"
+        return f"{self.getName()}, {self.__diameter} inch, ${self.getPrice():.2f}"
 
 
 def main():
     # TODO Write your main program code here...
     print("~~ Welcome to the Pizza Shop ~~")
+    pizzaBase1 = PizzaBase('thin crust', 12, 10)
+    
+    print(pizzaBase1.clone().getDiameter())
+
     print()
 
 
--- test_driver.py
+++ test_driver.py
@@ -6,7 +6,73 @@
 # This is my own work as defined by the 
 # Academic Integrity policy. 
 
-import pizza_shop
-# TODO Import unittest/pytest
+from pizza_shop import PizzaBase
+import unittest
 
-#TODO Write a unittest or pytest for PizzaBase here...
+class TestPizzaBase(unittest.TestCase):
+
+    def testClone(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        pb2 = pb1.clone()
+        self.assertEqual(pb1.getName(), pb2.getName())
+        self.assertEqual(pb1.getPrice(), pb2.getPrice())
+        self.assertEqual(pb1.getDiameter(), pb2.getDiameter())
+
+        with self.assertRaises(AttributeError):
+            pb3 = pb1.clone(1)
+
+    def testEqualCheck(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        pb2 = PizzaBase("thin crust", 8.99, 12)
+        pb3 = PizzaBase("cheese crust", 9.99, 14)
+
+        self.assertTrue(pb1.equalCheck(pb2))
+        self.assertFalse(pb1.equalCheck(pb3))
+        with self.assertRaises(AttributeError):
+            pb1.equalCheck("not a pizza")
+
+    def testCostPerSquareInch(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        self.assertAlmostEqual(pb1.costPerSquareInch(), 0.198, places=3)
+
+        with self.assertRaises(ZeroDivisionError):
+            pb2 = PizzaBase("cheese crust", 0, 12)
+            pb2.costPerSquareInch()
+
+    def testGetDiameter(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        self.assertEqual(pb1.getDiameter(), 12)
+
+        with self.assertRaises(AttributeError):
+            pb2 = PizzaBase("cheese crust", 9.99, 14)
+            pb2.getDiameter()
+
+    def testSetDiameter(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        pb1.setDiameter(10)
+        self.assertAlmostEqual(pb1.getPrice(), 6.37, places=2)
+
+        with self.assertRaises(ValueError):
+            pb2 = PizzaBase("cheese crust", 9.99, 14)
+            pb2.setDiameter(20)
+
+    def testSetSize(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        pb1.setSize("small")
+        self.assertAlmostEqual(pb1.getPrice(), 3.99, places=2)
+
+        with self.assertRaises(ValueError):
+            pb2 = PizzaBase("cheese crust", 9.99, 14)
+            pb2.setSize("extra large")
+
+    def testStr(self):
+        pb1 = PizzaBase("thin crust", 8.99, 12)
+        self.assertEqual(str(pb1), "thin crust, 12 inch, $8.99")
+
+        with self.assertRaises(AttributeError):
+            pb2 = PizzaBase("cheese crust", 9.99, 14)
+            str(pb2)
+
+        
+if __name__ == "__main__":
+    unittest.main()
